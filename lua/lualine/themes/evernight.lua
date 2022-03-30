313c222f477da9ef1639bc7eb063799e3b5860f3
jasonzie <jasonzie.wu@gmail.com>
2022-03-31 01:32:23 +0700

    :hammer: Fix foreground in lualine

--- lua/lualine/themes/evernight.lua
+++ lua/lualine/themes/evernight.lua
@@ -4,23 +4,23 @@ local colors = require("evernight.colors").setup(config)
 local evernight = {}
 
 evernight.normal = {
-  a = { bg = colors.blue, fg = colors.gray0 },
+  a = { bg = colors.blue, fg = colors.bg },
   b = { bg = colors.bg2, fg = colors.blue },
   c = { bg = colors.bg1, fg = colors.fgd },
 }
 
 evernight.insert = {
-  a = { bg = colors.green, fg = colors.gray0 },
+  a = { bg = colors.green, fg = colors.bg },
   b = { bg = colors.bg2, fg = colors.green },
 }
 
 evernight.command = {
-  a = { bg = colors.yellow, fg = colors.gray0 },
+  a = { bg = colors.yellow, fg = colors.bg },
   b = { bg = colors.bg2, fg = colors.yellow },
 }
 
 evernight.visual = {
-  a = { bg = colors.magenta, fg = colors.gray0 },
+  a = { bg = colors.magenta, fg = colors.bg },
   b = { bg = colors.bg2, fg = colors.magenta },
 }
 
