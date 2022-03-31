52e8c54356a98865a2bfdadf841a68069c9d5861
jasonzie <jasonzie.wu@gmail.com>
2022-03-31 07:43:00 +0700

    :hammer: Change font weight and normal color lualine

--- lua/lualine/themes/evernight.lua
+++ lua/lualine/themes/evernight.lua
@@ -4,34 +4,34 @@ local colors = require("evernight.colors").setup(config)
 local evernight = {}
 
 evernight.normal = {
-  a = { bg = colors.blue, fg = colors.bg },
+  a = { bg = colors.cyan, fg = colors.bg, gui = "bold" },
   b = { bg = colors.bg2, fg = colors.blue },
   c = { bg = colors.bg1, fg = colors.fgd },
 }
 
 evernight.insert = {
-  a = { bg = colors.green, fg = colors.bg },
+  a = { bg = colors.green, fg = colors.bg, gui = "bold" },
   b = { bg = colors.bg2, fg = colors.green },
 }
 
 evernight.command = {
-  a = { bg = colors.yellow, fg = colors.bg },
+  a = { bg = colors.yellow, fg = colors.bg, gui = "bold" },
   b = { bg = colors.bg2, fg = colors.yellow },
 }
 
 evernight.visual = {
-  a = { bg = colors.magenta, fg = colors.bg },
+  a = { bg = colors.magenta, fg = colors.bg, gui = "bold" },
   b = { bg = colors.bg2, fg = colors.magenta },
 }
 
 evernight.replace = {
-  a = { bg = colors.red, fg = colors.black },
+  a = { bg = colors.red, fg = colors.gray0, gui = "bold" },
   b = { bg = colors.bg2, fg = colors.red },
 }
 
 evernight.inactive = {
-  a = { bg = colors.bg1, fg = colors.blue },
-  b = { bg = colors.bg1, fg = colors.bg2, gui = "bold" },
+  a = { bg = colors.bg1, fg = colors.blue, gui = "bold" },
+  b = { bg = colors.bg1, fg = colors.bg2 },
   c = { bg = colors.bg1, fg = colors.bg2 },
 }
 
